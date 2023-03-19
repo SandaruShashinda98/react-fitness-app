44458867eb585a5643eb6a9b9fc05556b6241dc5
Sandaru_shashinda <sandaru@echonlabs.com>
2023-03-19 23:35:10 +0530

    first commit

---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': '2da67c9c98msh303c37582acc044p116afajsnef24c1ed5e7a',
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': '2da67c9c98msh303c37582acc044p116afajsnef24c1ed5e7a',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
